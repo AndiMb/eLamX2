9f0d790a5d403d7a4eeb09157ea28bfef17e226a
Andreas Hauffe <andreas@hauffe-mail.de>
2024-03-10 11:54:51 +0100

    + Weiterentwicklung DerivedMaterial

--- File_View/src/de/elamx/fileview/nodes/DerivedMaterialNode.java
+++ File_View/src/de/elamx/fileview/nodes/DerivedMaterialNode.java
@@ -4,14 +4,7 @@
  */
 package de.elamx.fileview.nodes;
 
-import de.elamx.core.GlobalProperties;
-import de.elamx.core.propertyeditor.DensityPropertyEditorSupport;
 import de.elamx.core.propertyeditor.DoublePropertyEditorSupport;
-import de.elamx.core.propertyeditor.HygrothermCoeffPropertyEditorSupport;
-import de.elamx.core.propertyeditor.PoissonRatioPropertyEditorSupport;
-import de.elamx.core.propertyeditor.YieldStressPropertyEditorSupport;
-import de.elamx.core.propertyeditor.YoungsModulusPropertyEditorSupport;
-import de.elamx.fileview.nodes.DefaultMaterialNode.MaterialProperty;
 import de.elamx.laminate.DefaultMaterial;
 import de.elamx.laminate.DerivedMaterial;
 import de.elamx.laminate.Material;
@@ -21,10 +14,7 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyEditor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.ResourceBundle;
-import java.util.Set;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.PropertySupport;
@@ -99,135 +89,13 @@ public class DerivedMaterialNode extends AbstractNode implements PropertyChangeL
             nameProp.setShortDescription(bundle.getString("DefaultMaterial.NAME.description"));
             generalProp.put(nameProp);
 
-            myPropRef<Double> rhoProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RHO);
+            /*myPropRef<Double> rhoProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RHO);
             rhoProp.setName(DefaultMaterial.PROP_RHO);
             rhoProp.setDisplayName(bundle.getString("DefaultMaterial.DENSITY"));
             rhoProp.setShortDescription(bundle.getString("DefaultMaterial.DENSITY.description"));
             rhoProp.setHtmlName(bundle.getString("DefaultMaterial.DENSITY.html"));
             rhoProp.setPropertyEditorClass(DensityPropertyEditorSupport.class);
             generalProp.put(rhoProp);
-
-            myPropRef<Double> e1Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_EPAR);
-            e1Prop.setName(DefaultMaterial.PROP_EPAR);
-            e1Prop.setDisplayName(bundle.getString("DefaultMaterial.EPARALLEL"));
-            e1Prop.setShortDescription(bundle.getString("DefaultMaterial.EPARALLEL.description"));
-            e1Prop.setHtmlName(bundle.getString("DefaultMaterial.EPARALLEL.html"));
-            e1Prop.setPropertyEditorClass(YoungsModulusPropertyEditorSupport.class);
-            stiffProp.put(e1Prop);
-
-            myPropRef<Double> e2Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_ENOR);
-            e2Prop.setName(DefaultMaterial.PROP_ENOR);
-            e2Prop.setDisplayName(bundle.getString("DefaultMaterial.EPERPENDICULAR"));
-            e2Prop.setShortDescription(bundle.getString("DefaultMaterial.EPERPENDICULAR.description"));
-            e2Prop.setHtmlName(bundle.getString("DefaultMaterial.EPERPENDICULAR.html"));
-            e2Prop.setPropertyEditorClass(YoungsModulusPropertyEditorSupport.class);
-            stiffProp.put(e2Prop);
-
-            myPropRef<Double> nue12Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_NUE12);
-            nue12Prop.setName(DefaultMaterial.PROP_NUE12);
-            nue12Prop.setDisplayName(bundle.getString("DefaultMaterial.POISSONRATIO"));
-            nue12Prop.setShortDescription(bundle.getString("DefaultMaterial.POISSONRATIO.description"));
-            nue12Prop.setHtmlName(bundle.getString("DefaultMaterial.POISSONRATIO.html"));
-            nue12Prop.setPropertyEditorClass(PoissonRatioPropertyEditorSupport.class);
-            stiffProp.put(nue12Prop);
-
-            myPropRef<Double> gProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_G);
-            gProp.setName(DefaultMaterial.PROP_G);
-            gProp.setDisplayName(bundle.getString("DefaultMaterial.SHEARMODULUS"));
-            gProp.setShortDescription(bundle.getString("DefaultMaterial.SHEARMODULUS.description"));
-            gProp.setHtmlName(bundle.getString("DefaultMaterial.SHEARMODULUS.html"));
-            gProp.setPropertyEditorClass(YoungsModulusPropertyEditorSupport.class);
-            stiffProp.put(gProp);
-            
-            if (GlobalProperties.getDefault().isShowTransShear()){
-                myPropRef<Double> g13Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_G13);
-                g13Prop.setName(DefaultMaterial.PROP_G13);
-                g13Prop.setDisplayName(bundle.getString("DefaultMaterial.SHEARMODULUS13"));
-                g13Prop.setShortDescription(bundle.getString("DefaultMaterial.SHEARMODULUS13.description"));
-                g13Prop.setHtmlName(bundle.getString("DefaultMaterial.SHEARMODULUS13.html"));
-                g13Prop.setPropertyEditorClass(YoungsModulusPropertyEditorSupport.class);
-                stiffProp.put(g13Prop);
-                
-                myPropRef<Double> g23Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_G23);
-                g23Prop.setName(DefaultMaterial.PROP_G23);
-                g23Prop.setDisplayName(bundle.getString("DefaultMaterial.SHEARMODULUS23"));
-                g23Prop.setShortDescription(bundle.getString("DefaultMaterial.SHEARMODULUS23.description"));
-                g23Prop.setHtmlName(bundle.getString("DefaultMaterial.SHEARMODULUS23.html"));
-                g23Prop.setPropertyEditorClass(YoungsModulusPropertyEditorSupport.class);
-                stiffProp.put(g23Prop);
-            }
-
-            myPropRef<Double> alpha1Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_ALPHATPAR);
-            alpha1Prop.setName(DefaultMaterial.PROP_ALPHATPAR);
-            alpha1Prop.setDisplayName(bundle.getString("DefaultMaterial.ALPHAPARALLEL"));
-            alpha1Prop.setShortDescription(bundle.getString("DefaultMaterial.ALPHAPARALLEL.description"));
-            alpha1Prop.setHtmlName(bundle.getString("DefaultMaterial.ALPHAPARALLEL.html"));
-            alpha1Prop.setPropertyEditorClass(HygrothermCoeffPropertyEditorSupport.class);
-            hygtProp.put(alpha1Prop);
-
-            myPropRef<Double> alpha2Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_ALPHATNOR);
-            alpha2Prop.setName(DefaultMaterial.PROP_ALPHATNOR);
-            alpha2Prop.setDisplayName(bundle.getString("DefaultMaterial.ALPHAPERPENDICULAR"));
-            alpha2Prop.setShortDescription(bundle.getString("DefaultMaterial.ALPHAPERPENDICULAR.description"));
-            alpha2Prop.setHtmlName(bundle.getString("DefaultMaterial.ALPHAPERPENDICULAR.html"));
-            alpha2Prop.setPropertyEditorClass(HygrothermCoeffPropertyEditorSupport.class);
-            hygtProp.put(alpha2Prop);
-
-            myPropRef<Double> beta1Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_BETAPAR);
-            beta1Prop.setName(DefaultMaterial.PROP_BETAPAR);
-            beta1Prop.setDisplayName(bundle.getString("DefaultMaterial.BETAPARALLEL"));
-            beta1Prop.setShortDescription(bundle.getString("DefaultMaterial.BETAPARALLEL.description"));
-            beta1Prop.setHtmlName(bundle.getString("DefaultMaterial.BETAPARALLEL.html"));
-            beta1Prop.setPropertyEditorClass(HygrothermCoeffPropertyEditorSupport.class);
-            hygtProp.put(beta1Prop);
-
-            myPropRef<Double> beta2Prop = new myPropRef<>(material, double.class, DefaultMaterial.PROP_BETANOR);
-            beta2Prop.setName(DefaultMaterial.PROP_BETANOR);
-            beta2Prop.setDisplayName(bundle.getString("DefaultMaterial.BETAPERPENDICULAR"));
-            beta2Prop.setShortDescription(bundle.getString("DefaultMaterial.BETAPERPENDICULAR.description"));
-            beta2Prop.setHtmlName(bundle.getString("DefaultMaterial.BETAPERPENDICULAR.html"));
-            beta2Prop.setPropertyEditorClass(HygrothermCoeffPropertyEditorSupport.class);
-            hygtProp.put(beta2Prop);
-
-            myPropRef<Double> rpartenProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RPARTEN);
-            rpartenProp.setName(DefaultMaterial.PROP_RPARTEN);
-            rpartenProp.setDisplayName(bundle.getString("DefaultMaterial.RPARTEN"));
-            rpartenProp.setShortDescription(bundle.getString("DefaultMaterial.RPARTEN.description"));
-            rpartenProp.setHtmlName(bundle.getString("DefaultMaterial.RPARTEN.html"));
-            rpartenProp.setPropertyEditorClass(YieldStressPropertyEditorSupport.class);
-            strengthProp.put(rpartenProp);
-
-            myPropRef<Double> rparcomProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RPARCOM);
-            rparcomProp.setName(DefaultMaterial.PROP_RPARCOM);
-            rparcomProp.setDisplayName(bundle.getString("DefaultMaterial.RPARCOM"));
-            rparcomProp.setShortDescription(bundle.getString("DefaultMaterial.RPARCOM.description"));
-            rparcomProp.setHtmlName(bundle.getString("DefaultMaterial.RPARCOM.html"));
-            rparcomProp.setPropertyEditorClass(YieldStressPropertyEditorSupport.class);
-            strengthProp.put(rparcomProp);
-
-            myPropRef<Double> rnortenProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RNORTEN);
-            rnortenProp.setName(DefaultMaterial.PROP_RNORTEN);
-            rnortenProp.setDisplayName(bundle.getString("DefaultMaterial.RNORTEN"));
-            rnortenProp.setShortDescription(bundle.getString("DefaultMaterial.RNORTEN.description"));
-            rnortenProp.setHtmlName(bundle.getString("DefaultMaterial.RNORTEN.html"));
-            rnortenProp.setPropertyEditorClass(YieldStressPropertyEditorSupport.class);
-            strengthProp.put(rnortenProp);
-
-            myPropRef<Double> rnorcomProp = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RNORCOM);
-            rnorcomProp.setName(DefaultMaterial.PROP_RNORCOM);
-            rnorcomProp.setDisplayName(bundle.getString("DefaultMaterial.RNORCOM"));
-            rnorcomProp.setShortDescription(bundle.getString("DefaultMaterial.RNORCOM.description"));
-            rnorcomProp.setHtmlName(bundle.getString("DefaultMaterial.RNORCOM.html"));
-            rnorcomProp.setPropertyEditorClass(YieldStressPropertyEditorSupport.class);
-            strengthProp.put(rnorcomProp);
-
-            myPropRef<Double> rshear = new myPropRef<>(material, double.class, DefaultMaterial.PROP_RSHEAR);
-            rshear.setName(DefaultMaterial.PROP_RSHEAR);
-            rshear.setDisplayName(bundle.getString("DefaultMaterial.RSHEAR"));
-            rshear.setShortDescription(bundle.getString("DefaultMaterial.RSHEAR.description"));
-            rshear.setHtmlName(bundle.getString("DefaultMaterial.RSHEAR.html"));
-            rshear.setPropertyEditorClass(YieldStressPropertyEditorSupport.class);
-            strengthProp.put(rshear);
 
             Set<String> addValKeySet = material.getAdditionalValueKeySet();
             MaterialProperty[] mPs = new MaterialProperty[addValKeySet.size()];
@@ -253,7 +121,7 @@ public class DerivedMaterialNode extends AbstractNode implements PropertyChangeL
 
             for (MaterialProperty mP : mPs) {
                 additionalProp.put(mP);
-            }
+            }*/
         } catch (NoSuchMethodException ex) {
             Exceptions.printStackTrace(ex);
         }
