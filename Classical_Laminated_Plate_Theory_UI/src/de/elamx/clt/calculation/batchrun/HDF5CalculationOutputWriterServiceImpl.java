6fa9dddbe3aa099d5b1148d7e1ac48ed9a126556
FloDX <85361553+FloDX@users.noreply.github.com>
2024-07-11 19:03:48 +0200

    HDF5-Ausgabe: Formatanpassung der Ausgabe von Calculation Ergebnissen;

--- Classical_Laminated_Plate_Theory_UI/src/de/elamx/clt/calculation/batchrun/HDF5CalculationOutputWriterServiceImpl.java
+++ Classical_Laminated_Plate_Theory_UI/src/de/elamx/clt/calculation/batchrun/HDF5CalculationOutputWriterServiceImpl.java
@@ -25,11 +25,15 @@
  */
 package de.elamx.clt.calculation.batchrun;
 
+import ch.systemsx.cisd.hdf5.CompoundElement;
+import ch.systemsx.cisd.hdf5.HDF5CompoundType;
 import ch.systemsx.cisd.hdf5.IHDF5Writer;
 import de.elamx.clt.CLT_LayerResult;
 import de.elamx.clt.Loads;
 import de.elamx.clt.Strains;
 import de.elamx.clt.calculation.CalculationModuleData;
+import java.util.ArrayList;
+import java.util.List;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
@@ -50,101 +54,243 @@ public class HDF5CalculationOutputWriterServiceImpl implements HDF5CalculationOu
 
         double[] forces = loads.getForceMomentAsVector();
 
-        hdf5writer.object().createGroup(groupName.concat("/mechanical loads"));
-        hdf5writer.float64().write(groupName.concat("/mechanical loads/nxx"), forces[0]);
-        hdf5writer.float64().write(groupName.concat("/mechanical loads/nyy"), forces[1]);
-        hdf5writer.float64().write(groupName.concat("/mechanical loads/nxy"), forces[2]);
-        hdf5writer.float64().write(groupName.concat("/mechanical loads/mxx"), forces[3]);
-        hdf5writer.float64().write(groupName.concat("/mechanical loads/myy"), forces[4]);
-        hdf5writer.float64().write(groupName.concat("/mechanical loads/mxy"), forces[5]);
+        ArrayList<Double> mechanicalLoadsValuesArrayList = new ArrayList<>();
+        ArrayList<String> mechanicalLoadsNamesArrayList = new ArrayList<>();
 
-        hdf5writer.object().createGroup(groupName.concat("/hygrothermal loads"));
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/nxx"), loads.getnT_x());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/nyy"), loads.getnT_y());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/nxy"), loads.getnT_xy());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/mxx"), loads.getmT_x());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/myy"), loads.getmT_y());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/mxy"), loads.getmT_xy());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/deltaT"), loads.getDeltaT());
-        hdf5writer.float64().write(groupName.concat("/hygrothermal loads/deltac"), loads.getDeltaH());
+        mechanicalLoadsValuesArrayList.add(forces[0]);
+        mechanicalLoadsNamesArrayList.add("nxx");
+
+        mechanicalLoadsValuesArrayList.add(forces[1]);
+        mechanicalLoadsNamesArrayList.add("nyy");
+
+        mechanicalLoadsValuesArrayList.add(forces[2]);
+        mechanicalLoadsNamesArrayList.add("nxy");
+
+        mechanicalLoadsValuesArrayList.add(forces[3]);
+        mechanicalLoadsNamesArrayList.add("mxx");
+
+        mechanicalLoadsValuesArrayList.add(forces[4]);
+        mechanicalLoadsNamesArrayList.add("myy");
+
+        mechanicalLoadsValuesArrayList.add(forces[5]);
+        mechanicalLoadsNamesArrayList.add("mxy");
+
+        HDF5CompoundType<List<?>> mechanicalLoadsType
+                = hdf5writer.compound().getInferredType("Mechanical loads", mechanicalLoadsNamesArrayList, mechanicalLoadsValuesArrayList);
+
+        hdf5writer.compound().write(groupName.concat("/mechanical loads"), mechanicalLoadsType, mechanicalLoadsValuesArrayList);
+
+        ArrayList<Double> hygrothermalLoadsValuesArrayList = new ArrayList<>();
+        ArrayList<String> hygrothermalLoadsNamesArrayList = new ArrayList<>();
+
+        hygrothermalLoadsValuesArrayList.add(loads.getnT_x());
+        hygrothermalLoadsNamesArrayList.add("nxx");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getnT_y());
+        hygrothermalLoadsNamesArrayList.add("nyy");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getnT_xy());
+        hygrothermalLoadsNamesArrayList.add("nxy");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getmT_x());
+        hygrothermalLoadsNamesArrayList.add("mxx");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getmT_y());
+        hygrothermalLoadsNamesArrayList.add("myy");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getmT_xy());
+        hygrothermalLoadsNamesArrayList.add("mxy");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getDeltaT());
+        hygrothermalLoadsNamesArrayList.add("deltaT");
+
+        hygrothermalLoadsValuesArrayList.add(loads.getDeltaH());
+        hygrothermalLoadsNamesArrayList.add("deltac");
+
+        HDF5CompoundType<List<?>> hygrothermalLoadsType
+                = hdf5writer.compound().getInferredType("Hygrothermal loads", hygrothermalLoadsNamesArrayList, hygrothermalLoadsValuesArrayList);
+
+        hdf5writer.compound().write(groupName.concat("/hygrothermal loads"), hygrothermalLoadsType, hygrothermalLoadsValuesArrayList);
 
         double[] strains = strain.getEpsilonKappaAsVector();
-        hdf5writer.object().createGroup(groupName.concat("/global strains"));
-        hdf5writer.float64().write(groupName.concat("/global strains/exx"), strains[0]);
-        hdf5writer.float64().write(groupName.concat("/global strains/eyy"), strains[1]);
-        hdf5writer.float64().write(groupName.concat("/global strains/gxy"), strains[2]);
-        hdf5writer.float64().write(groupName.concat("/global strains/kxx"), strains[3]);
-        hdf5writer.float64().write(groupName.concat("/global strains/kyy"), strains[4]);
-        hdf5writer.float64().write(groupName.concat("/global strains/kxy"), strains[5]);
+
+        ArrayList<Double> globalStrainsValuesArrayList = new ArrayList<>();
+        ArrayList<String> globalStrainsNamesArrayList = new ArrayList<>();
+
+        globalStrainsValuesArrayList.add(strains[0]);
+        globalStrainsNamesArrayList.add("exx");
+
+        globalStrainsValuesArrayList.add(strains[1]);
+        globalStrainsNamesArrayList.add("eyy");
+
+        globalStrainsValuesArrayList.add(strains[2]);
+        globalStrainsNamesArrayList.add("gxy");
+
+        globalStrainsValuesArrayList.add(strains[3]);
+        globalStrainsNamesArrayList.add("kxx");
+
+        globalStrainsValuesArrayList.add(strains[4]);
+        globalStrainsNamesArrayList.add("kyy");
+
+        globalStrainsValuesArrayList.add(strains[5]);
+        globalStrainsNamesArrayList.add("kxy");
+
+        HDF5CompoundType<List<?>> globalStrainsType
+                = hdf5writer.compound().getInferredType("Global strains", globalStrainsNamesArrayList, globalStrainsValuesArrayList);
+
+        hdf5writer.compound().write(groupName.concat("/global strains"), globalStrainsType, globalStrainsValuesArrayList);
 
         int layerResultsNum = results.length;
-
+        
         hdf5writer.object().createGroup(groupName.concat("/local layer results"));
-        hdf5writer.object().createGroup(groupName.concat("/local layer results/upper"));
-        hdf5writer.object().createGroup(groupName.concat("/local layer results/lower"));
-        hdf5writer.int32().setAttr(groupName.concat("/local layer results/upper"), "number of layers", layerResultsNum);
+
+        double s11;
+        double s22;
+        double s12;
+        double e11;
+        double e12;
+        double e22;
+        double RF;
+        
+        HDF5LocalLayerResult[] llres_lo = new HDF5LocalLayerResult[layerResultsNum];
+        for (int ii = 0; ii < layerResultsNum; ii++) {
+            s11 = results[ii].getSss_lower().getStress()[0];
+            s22 = results[ii].getSss_lower().getStress()[1];
+            s12 = results[ii].getSss_lower().getStress()[2];
+            e11 = results[ii].getSss_lower().getStrain()[0];
+            e22 = results[ii].getSss_lower().getStrain()[1];
+            e12 = results[ii].getSss_lower().getStrain()[2];
+            RF  = results[ii].getRr_upper().getMinimalReserveFactor();
+            llres_lo[ii] = new HDF5LocalLayerResult(s11, s22, s12, e11, e22, e12, RF);
+        }
+        hdf5writer.compound().writeArray(groupName.concat("/local layer results/lower"), llres_lo);
         hdf5writer.int32().setAttr(groupName.concat("/local layer results/lower"), "number of layers", layerResultsNum);
-
-        double[] s11 = new double[layerResultsNum];
-        double[] s22 = new double[layerResultsNum];
-        double[] s12 = new double[layerResultsNum];
-        double[] e11 = new double[layerResultsNum];
-        double[] e12 = new double[layerResultsNum];
-        double[] e22 = new double[layerResultsNum];
-        double[] RF = new double[layerResultsNum];
+        
+        HDF5LocalLayerResult[] llres_up = new HDF5LocalLayerResult[layerResultsNum];
         for (int ii = 0; ii < layerResultsNum; ii++) {
-            s11[ii] = results[ii].getSss_upper().getStress()[0];
-            s22[ii] = results[ii].getSss_upper().getStress()[1];
-            s12[ii] = results[ii].getSss_upper().getStress()[2];
-            e11[ii] = results[ii].getSss_upper().getStrain()[0];
-            e22[ii] = results[ii].getSss_upper().getStrain()[1];
-            e12[ii] = results[ii].getSss_upper().getStrain()[2];
-            RF[ii] = results[ii].getRr_upper().getMinimalReserveFactor();
+            s11 = results[ii].getSss_upper().getStress()[0];
+            s22 = results[ii].getSss_upper().getStress()[1];
+            s12 = results[ii].getSss_upper().getStress()[2];
+            e11 = results[ii].getSss_upper().getStrain()[0];
+            e22 = results[ii].getSss_upper().getStrain()[1];
+            e12 = results[ii].getSss_upper().getStrain()[2];
+            RF  = results[ii].getRr_upper().getMinimalReserveFactor();
+            llres_up[ii] = new HDF5LocalLayerResult(s11, s22, s12, e11, e22, e12, RF);
         }
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/s11"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/s11"), s11);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/s12"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/s12"), s12);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/s22"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/s22"), s22);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/e11"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/e11"), e11);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/e22"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/e22"), e22);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/e12"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/e12"), e12);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/upper/RF"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/upper/RF"), RF);
+        hdf5writer.compound().writeArray(groupName.concat("/local layer results/upper"), llres_up);
+        hdf5writer.int32().setAttr(groupName.concat("/local layer results/upper"), "number of layers", layerResultsNum);
+    }
 
-        s11 = new double[layerResultsNum];
-        s22 = new double[layerResultsNum];
-        s12 = new double[layerResultsNum];
-        e11 = new double[layerResultsNum];
-        e12 = new double[layerResultsNum];
-        e22 = new double[layerResultsNum];
-        for (int ii = 0; ii < layerResultsNum; ii++) {
-            s11[ii] = results[ii].getSss_lower().getStress()[0];
-            s22[ii] = results[ii].getSss_lower().getStress()[1];
-            s12[ii] = results[ii].getSss_lower().getStress()[2];
-            e11[ii] = results[ii].getSss_lower().getStrain()[0];
-            e22[ii] = results[ii].getSss_lower().getStrain()[1];
-            e12[ii] = results[ii].getSss_lower().getStrain()[2];
-            RF[ii] = results[ii].getRr_lower().getMinimalReserveFactor();
+    /**
+     * A HDF5 Data Transfer Object for local layer results.
+     */
+    static class HDF5LocalLayerResult
+    {
+        // Include the unit in the member name
+        @CompoundElement(memberName = "s11")
+        private double s11;
+
+        // Include the unit in the member name
+        @CompoundElement(memberName = "s22")
+        private double s22;
+
+        // Include the unit in the member name
+        @CompoundElement(memberName = "s12")
+        private double s12;
+
+        // Include the unit in the member name
+        @CompoundElement(memberName = "e11")
+        private double e11;
+
+        // Include the unit in the member name
+        @CompoundElement(memberName = "e22")
+        private double e22;
+
+        // Include the unit in the member name
+        @CompoundElement(memberName = "e12")
+        private double e12;
+
+        // Include the unit in the member name
+        @CompoundElement(memberName = "RF")
+        private double RF;
+
+        // Important: needs to have a default constructor, otherwise JHDF5 will bail out on reading.
+        HDF5LocalLayerResult()
+        {
         }
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/s11"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/s11"), s11);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/s12"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/s12"), s12);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/s22"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/s22"), s22);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/e11"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/e11"), e11);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/e22"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/e22"), e22);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/e12"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/e12"), e12);
-        hdf5writer.float64().createArray(groupName.concat("/local layer results/lower/RF"), layerResultsNum);
-        hdf5writer.float64().writeArray(groupName.concat("/local layer results/lower/RF"), RF);
+
+        HDF5LocalLayerResult(double s11, double s22, double s12, double e11, double e22, double e12, double RF)
+        {
+            this.s11 = s11;
+            this.s22 = s22;
+            this.s12 = s12;
+            this.e11 = e11;
+            this.e22 = e22;
+            this.e12 = e12;
+            this.RF  = RF;
+        }
+
+        public double getS11() {
+            return s11;
+        }
+
+        public void setS11(double s11) {
+            this.s11 = s11;
+        }
+
+        public double getS22() {
+            return s22;
+        }
+
+        public void setS22(double s22) {
+            this.s22 = s22;
+        }
+
+        public double getS12() {
+            return s12;
+        }
+
+        public void setS12(double s12) {
+            this.s12 = s12;
+        }
+
+        public double getE11() {
+            return e11;
+        }
+
+        public void setE11(double e11) {
+            this.e11 = e11;
+        }
+
+        public double getE22() {
+            return e22;
+        }
+
+        public void setE22(double e22) {
+            this.e22 = e22;
+        }
+
+        public double getE12() {
+            return e12;
+        }
+
+        public void setE12(double e12) {
+            this.e12 = e12;
+        }
+
+        public double getRF() {
+            return RF;
+        }
+
+        public void setRF(double RF) {
+            this.RF = RF;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "LocalLayerResult [s11=" + s11 + ", s22=" + s22 + ", s12=" + s12 + "e11" + e11 + ", e22=" + e22 + "e12" + e12 + ", RF" + RF + "]";
+        }
 
     }
 
